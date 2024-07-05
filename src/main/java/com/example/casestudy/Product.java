bbbbf3f6be473525af407d946fb1f23713ddd466
Seitama179 <ngochd997@gmail.com>
2024-07-05 09:51:11 +0700

    up file

---
+++ src/main/java/com/example/casestudy/Product.java
@@ -0,0 +1,18 @@
+package com.example.casestudy;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String image;
+    private String manufacturer;
+    private Double price;
+    private int quantity;
+    private float size;
+    private String color;
+    private int rom;
+    private int ram;
+    private String operatingSystem;
+    private int battery;
+    private float discount;
+
+}
